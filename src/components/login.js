249f435ee5392139ac06eb00280a60b3b8c722d2
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-02 23:12:28 -0400

    converted login component to redux-form

--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,74 @@
 import React from 'react';
+import { reduxForm, Field, focus } from 'redux-form';
+import { required, nonEmpty } from '../validators';
+
+import { login } from '../actions';
 
 import LandingNav from './landing-nav';
+import Input from './input';
 
 import './signup-form.css';
 
-export default class Login extends React.Component {
+export class Login extends React.Component {
+  onSubmit(values) {
+    return this.props.dispatch(login(values.user, values.pass));
+  }
+
   render() {
+    let error;
+    if(this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
+    }
+
     return (
       <div>
         <LandingNav />
         <main>
-          <form id="login-form">
+          <form
+            id="login-form"
+            onSubmit={this.props.handleSubmit(values =>
+              this.onSubmit(values)
+          )}>
             <h2>Log into your account</h2>
-            <input id="user" type="text" placeholder="Username" required />
-            <input id="pass" type="password" placeholder="Password" required />
-            <button id="login" type="submit">Continue</button>
+            {error}
+            <Field
+              name="user"
+              id="user"
+              type="text"
+              component={Input}
+              placeholder="Username"
+              validate={[required, nonEmpty]}
+            />
+            <Field
+              name="pass"
+              id="pass"
+              type="password"
+              component={Input}
+              placeholder="Password"
+              validate={[required, nonEmpty]}
+            />
+            <button
+              id="login"
+              type="submit"
+              disabled={
+                this.props.pristine || this.props.submitting
+              }
+            >
+              Continue
+            </button>
           </form>
         </main>
       </div>
     );
   }
 }
+
+export default reduxForm({
+  form: 'login',
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('login', 'username'))
+})(Login);
