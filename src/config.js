af6af9d95fe64331ce61eafd94900145ad5299a5
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-08 17:13:12 -0400

    attempted debug of build issue

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const PORT = process.env.PORT || 3000;  
+export const PORT = process.env.PORT || 3000;
 export const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
