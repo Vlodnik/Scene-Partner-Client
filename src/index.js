dafd8f6eccd916c7a5fdb16def4c621cc88b80b9
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-08 15:07:33 -0400

    configured different builds for dev and prod

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
+dotenv.config();
 
 ReactDOM.render(
   <Provider store={store}>
