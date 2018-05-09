5a154d25a842b3160eabe44a82a430db28abc6e6
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-09 15:38:56 -0400

    reinstalled create-react-app dependencies completely

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
-dotenv.config();
 
 ReactDOM.render(
   <Provider store={store}>
